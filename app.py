eb68bee4f63b1ce0b5746ec92184a1558189c154
dule007 <kristijan.pavicic96@gmail.com>
2022-04-20 14:54:05 +0200

    aded new changes 20.4 t2

--- app.py
+++ app.py
@@ -14,7 +14,15 @@ from urllib.request import urlopen
 
 with urlopen("https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson") as response:
  Brazil = json.load(response) # Javascrip object notation 
-print(Brazil)
+#print(Brazil)
+state_id_map = {}
+for feature in Brazil ['features']:
+
+    feature['id'] = feature['properties']['name']
+    state_id_map[feature['properties']['sigla']] = feature['id']
+
+
+print(state_id_map)
 # app = dash.Dash()
 # app.layout = html.Div([
 #     dcc.Graph(figure=fig)
